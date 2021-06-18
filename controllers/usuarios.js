1d7140f11350d100667efd869d491bfbaca10ce4
Oscar Alexis <oscar.flores5267@alumnos.udg.mx>
2021-06-18 12:41:24 -0500

    Primer commit

---
+++ controllers/usuarios.js
@@ -0,0 +1,55 @@
+const { response, request } = require('express');
+
+const usuariosGet = (req = request, res = response) => {
+    const query = req.query;
+
+    res.json({
+        ok: true,
+        msg: 'get Api - controlador',
+        query
+    });
+}
+
+const usuariosPost = (req, res = response) => {
+
+    const { nombre, edad } = req.body;
+
+    res.json({
+        ok: true,
+        msg: 'post Api - controlador',
+        nombre, 
+        edad
+    });
+}
+
+const usuariosPut = (req, res = response) => {
+    const id = req.params.id;
+    res.json({
+        ok: true,
+        msg: 'put Api - controlador',
+        id
+    });
+}
+
+const usuariosPatch = (req, res = response) => {
+    res.json({
+        ok: true,
+        msg: 'patch Api - controlador'
+    });
+}
+
+const usuariosDelete = (req, res = response) => {
+    res.json({
+        ok: true,
+        msg: 'delete Api - controlador'
+    });
+}
+
+
+module.exports = {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosPatch, 
+    usuariosDelete
+}
