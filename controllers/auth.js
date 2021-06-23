04b7b33bfeb3c3698db908e84126dd266a57b304
Oscar Alexis <oscar.flores5267@alumnos.udg.mx>
2021-06-23 10:33:33 -0500

    Google Sing added

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const { response } = require("express");
 const Usuario = require('../models/usuario');
 const bcriptjs = require('bcryptjs');
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async(req, res = response) => {
 
@@ -48,6 +49,55 @@ const login = async(req, res = response) => {
 
 }
 
+const googleSingin = async(req, res = response) => {
+
+    const { id_token } = req.body;
+
+    try{
+
+        const { nombre, img, correo } = await googleVerify( id_token );
+
+        let usuario = await Usuario.findOne({ correo });
+
+        if( !usuario ){
+            // Tengo que crearlo 
+            const data = {
+                nombre, 
+                correo, 
+                password: ':P',
+                img,
+                google: true
+            };
+
+            usuario = new Usuario( data );
+            await usuario.save();s
+        }
+
+        // Si el usuario en DB
+        if ( !usuario.estado ){
+            res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+
+        // Generar el JWT
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            usuario, 
+            token
+        });
+
+    }catch(error){
+        res.status(400).json({
+            msg: 'Token de Google no es válido'
+        });
+    }
+
+    
+}
+
 module.exports = {
-    login
+    login,
+    googleSingin
 }
