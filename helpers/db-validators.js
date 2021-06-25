3b32caa51cbf85765fead821eb073118890f9fdc
Oscar Alexis <oscar.flores5267@alumnos.udg.mx>
2021-06-25 13:06:58 -0500

    File Upload added

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -16,6 +16,7 @@ const emailExiste = async( correo = '' ) => {
     } 
 }
 
+/* Usuario por ID */
 const existeUsuarioPorId = async( id ) => {
     const existeUsuario = await Usuario.findById(id);
     if( !existeUsuario ){
@@ -39,11 +40,22 @@ const existeProductoPorId = async( id ) => {
     } 
 }
 
+/* VAlidar las colecciones permitidas */
+const coleccionesPermitidas = (coleccion = '', colecciones = [] ) => {
+
+    const incluida = colecciones.includes( coleccion );
+    if ( !incluida ){
+        throw new Error(`La coleccion ${ coleccion } no es permitida, ${ colecciones}`);
+    }
+
+    return true;
+}
 
 module.exports = {
     esRoleValido,
     emailExiste,
     existeUsuarioPorId,
     existeCategoriaPorId,
-    existeProductoPorId
+    existeProductoPorId,
+    coleccionesPermitidas
 }
--- models/server.js
+++ models/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const fileUpload = require('express-fileUpload');
 
 const { dbConnection } = require('../database/config');
 class Server {
@@ -14,6 +15,7 @@ class Server {
             categorias: '/api/categorias',
             productos: '/api/productos',
             usuarios: '/api/usuarios',
+            uploads: '/api/uploads',
         };
 
         // Conectar a Base de Datos
@@ -39,6 +41,13 @@ class Server {
 
         // Derectorio Público
         this.app.use(express.static('public'));
+
+        // Fileupload - Carga de archivos
+        this.app.use(fileUpload({
+            useTempFiles : true,
+            tempFileDir : '/tmp/',
+            createParentPath: true
+        }));
     }
 
     routes(){
@@ -47,6 +56,7 @@ class Server {
         this.app.use(this.paths.categorias , require('../routes/categorias'));
         this.app.use(this.paths.productos , require('../routes/productos'));
         this.app.use(this.paths.usuarios , require('../routes/usuarios'));
+        this.app.use(this.paths.uploads , require('../routes/uploads'));
     }
 
     listen(){
